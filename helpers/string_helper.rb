b3ccdb2f37fed619db2b19cca3c92bc414928410
Nguyen Bao Ngoc <ngocnguyenbao2707@gmail.com>
2019-04-28 22:31:44 +0700

    #12 refactor code in string helper, add assumptions into README

--- helpers/string_helper.rb
+++ helpers/string_helper.rb
@@ -2,23 +2,23 @@ class StringHelper
   def self.check_balance_parenthesis(string)
     raise ArgumentError, string.to_s + " is not a String" unless string.is_a? String
 
-    opening = { 0 => '[', 1 => '{', 2 => '(' }
-    closing = { 0 => ']', 1 => '}', 2 => ')' }
+    opening = %w|[ { (|
+    closing = %w|] } )|
 
     match = []
 
     string.split('').each do |c|
-      if opening.value? c
+      if opening.include? c
         # found the character in opening list
         # prepend it to the head of the match array
         match.prepend(c)
-      elsif closing.value? c
+      elsif closing.include? c
         # found the character in closing list
         # start checking
         if match.length.zero?
           # there's no opening parenthesis in match array yet
           return false
-        elsif match[0] == opening[closing.key(c)]
+        elsif match[0] == opening[closing.find_index(c)]
           # there's a matched open parenthesis with the close one
           # at the head of the matched array
           # remove the head
